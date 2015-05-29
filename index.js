2084530370902f3bd8629110e98b6f2fc1eee87d
Marc Dassonneville <reachtheflow@gmail.com>
2015-05-29 11:41:53 +0200

    clean

--- index.js
+++ index.js
@@ -9,24 +9,15 @@ var Scraper = require('./scraper.js');
 
 var scraper = new Scraper({
   requestsConcurrency: 2,
-  downloadsConcurrency: 4,
-  nocolorthief: true,
+  downloadsConcurrency: 2,
   colorthiefConcurrency: 1
 });
 
-// complete :
-//  minx:0 miny:45 maxx:6 maxy:47 step:1
-//  minx:6 miny:45 maxx:8 maxy:46 step:1
-//
-//  minx:0 miny:47 maxx:2 maxy:48 step:1
-//  minx:2 miny:47 maxx:4 maxy:48 step:1
-//  minx:4 miny:47 maxx:6 maxy:48 step:1
-//  minx:6 miny:47 maxx:8 maxy:48 step:1
 scraper.fetch({
-  minx: 6,
-  miny: 45,
-  maxx: 8,
-  maxy: 46,
+  minx: 0,
+  miny: 47,
+  maxx: 1,
+  maxy: 48,
   step: 1
 }).then(function () {
   console.log('finished !');
